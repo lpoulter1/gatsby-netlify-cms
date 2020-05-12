d4a7311261563e25f11ffba448696fbd03a17042
laurie <laurie@brandwatch.com>
2020-05-12 16:44:13 +0100

    Add full page video

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,76 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
+import IntroVideo from "../assets/homepageVideo.mp4"
+import "./site.css"
 
-export default () => <div>Hello world!</div>
+const PostLink = ({ post }) => (
+  <article className="card ">
+    <Link to={post.frontmatter.path}>
+      {!!post.frontmatter.thumbnail && (
+        <img
+          src={post.frontmatter.thumbnail}
+          alt={post.frontmatter.title + "- Featured Shot"}
+        />
+      )}
+    </Link>
+    <header>
+      <h2 className="post-title">
+        <Link to={post.frontmatter.path} className="post-link">
+          {post.frontmatter.title}
+        </Link>
+      </h2>
+      <div className="post-meta">{post.frontmatter.date}</div>
+    </header>
+  </article>
+)
+
+const IndexPage = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) => {
+  const Posts = edges
+    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+    .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
+
+  return (
+    <div>
+      <div>
+        <video
+          className="homepage-video"
+          muted
+          loop
+          autoPlay
+          src={IntroVideo}
+          type="video/mp4"
+        >
+          <p>Your browser does not support the video element.</p>
+        </video>
+      </div>
+
+      <div>
+        <h2>Blog Posts &darr;</h2>
+        <div className="grids">{Posts}</div>
+      </div>
+    </div>
+  )
+}
+
+export default IndexPage
+export const pageQuery = graphql`
+  query indexPageQuery {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`
