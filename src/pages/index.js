fbb846ca2423c87d1bfb8dd264449aa11c334826
laurie <laurie@brandwatch.com>
2020-05-22 19:32:12 +0100

    fix: check window is defined to fix gatsby build

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,10 +14,14 @@ import {
 import "./reset.css"
 import "./site.css"
 
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty("--vh", `${vh}px`)
+typeof window !== `undefined` ? setVH() : null
+
+function setVH(params) {
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+  let vh = window && window.innerHeight * 0.01
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty("--vh", `${vh}px`)
+}
 
 const Post = ({ post }) => (
   <article className="card ">
