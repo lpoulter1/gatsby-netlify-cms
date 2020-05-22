e1bcdea806af5b00df056d39970651f2a0aa41f3
laurie <laurie@brandwatch.com>
2020-05-22 20:06:33 +0100

    fix: esling warning

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,7 @@ import {
 import "./reset.css"
 import "./site.css"
 
+// eslint-disable-next-line
 typeof window !== `undefined` ? setVH() : null
 
 function setVH(params) {
@@ -95,7 +96,9 @@ const IndexPage = ({
         >
           <p>Your browser does not support the video element.</p>
         </video>
-        <SocialIcons />
+        <div>
+          <SocialIcons />
+        </div>
       </div>
 
       <div className="news-section">
