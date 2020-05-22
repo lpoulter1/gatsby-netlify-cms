0a91cfccf705c131139955886a47732e695fcb28
laurie <laurie@brandwatch.com>
2020-05-22 17:31:08 +0100

    Add social Icons

---
+++ src/icons/index.js
@@ -0,0 +1,6 @@
+export { default as Instagram } from "./Instagram"
+export { default as Facebook } from "./Facebook"
+export { default as Spotify } from "./Spotify"
+export { default as Youtube } from "./Youtube"
+export { default as Amazon } from "./Amazon"
+export { default as SoundCloud } from "./SoundCloud"
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,70 @@
 import React from "react"
 import { graphql } from "gatsby"
 import IntroVideo from "../assets/homepageVideo.mp4"
+import IntroVideoPreviewImage from "../assets/homepageVideoPreviewImage.jpg"
+import {
+  Instagram,
+  Facebook,
+  Spotify,
+  Youtube,
+  Amazon,
+  SoundCloud,
+} from "../icons"
+
 import "./reset.css"
 import "./site.css"
 
 const Post = ({ post }) => (
   <article className="card ">
-    <img
-      src={post.frontmatter.thumbnail}
-      alt={post.frontmatter.title + "- Featured Shot"}
-      width="200px"
-    />
-    <header>
-      <h2 className="post-title">{post.frontmatter.title}</h2>
-      <div className="post-meta">{post.frontmatter.date}</div>
-      <div className="post-meta">{post.frontmatter.html}</div>
-    </header>
+    <h2 className="post-title">{post.frontmatter.title}</h2>
+    {post.frontmatter.thumbnail && (
+      <img
+        src={post.frontmatter.thumbnail}
+        alt={post.frontmatter.title + "- Featured Shot"}
+        width="200px"
+      />
+    )}
+    <div className="post-meta">{post.frontmatter.date}</div>
+    <div className="post-meta">{post.excerpt}</div>
   </article>
 )
 
+function SocialIcons(params) {
+  return (
+    <ul className="social-icons">
+      <li>
+        <a href="https://www.facebook.com/therapports/">
+          <Facebook width="1.5rem" />
+        </a>
+      </li>
+      <li>
+        <a href="https://www.instagram.com/therapports/">
+          <Instagram width="1.5rem" />
+        </a>
+      </li>
+      <li>
+        <a href="https://www.youtube.com/channel/UCDUavfXgJ6pCaS-KgrjVxig">
+          <Youtube width="1.5rem" />
+        </a>
+      </li>
+      <li>
+        <a href="https://open.spotify.com/album/6Q74wQiIvhluVWh7PfJIxr?si=2diemmM-Tdqx2al0ZH79Pw">
+          <Spotify width="1.5rem" />
+        </a>
+      </li>
+      <li>
+        <a href="https://www.amazon.co.uk/Come-Home-Rapports/dp/B0719N9ZQB">
+          <Amazon width="1.5rem" />
+        </a>
+      </li>
+      <li>
+        <a href="https://soundcloud.com/therapports">
+          <SoundCloud width="1.5rem" />
+        </a>
+      </li>
+    </ul>
+  )
+}
 const IndexPage = ({
   data: {
     allMarkdownRemark: { edges },
@@ -34,17 +80,17 @@ const IndexPage = ({
         <video
           className="homepage-video"
           muted
-          loop
           autoPlay
           src={IntroVideo}
+          poster={IntroVideoPreviewImage}
           type="video/mp4"
         >
           <p>Your browser does not support the video element.</p>
         </video>
+        <SocialIcons />
       </div>
 
       <div className="news-section">
-        <h2>News</h2>
         <div className="news-posts">{Posts}</div>
       </div>
     </div>
